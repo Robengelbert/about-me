bd54037e82f591046d8b0a555cbf55e644c11ed4
Robert Engelbert <Robe32002@yahoo.com>
2016-07-13 08:12:55 -0600

    Add basic html structure

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>about-me</title>
+		<link href="css/style.css"/>
+	</head>
+	<body>
+		<div id="head">
+		<header>
+			<h1>Robert Engelbert</h1>
+		</header>
+		</div>
+		<div class="wrapper">
+			<h2>Things I like</h2>
+			<p>I like many things for example: I like</p>
+			<ul>
+				<li>Biking</li>
+				<li>Hiking</li>
+				<li>Eating</li>
+				<li>Sleeping</li>
+			</ul>
+			<h2>Places I've been</h2>
+			<p>I've been a few places, and I do mean a few. The places I've been are:</p>
+			<ul>
+				<li>Wichita,Ks</li>
+				<li>Kansas City,Mo</li>
+				<li>Tulsa,Ok</li>
+				<li>Denver,Co</li>
+				<li>Colorado Springs,Co</li>
+				<li>Fayettville,Ar</li>
+				<li>Albuquerque,Nm</li>
+			</ul>
+			<h2>Things I want to do</h2>
+			<p>I want to do many thing's, here are a few:</p>
+			<ul>
+				<li>Ride the divide</li>
+				<li>Go to Alaska</li>
+				<li>Tour the west coast</li>
+				<li>Watch my kids grow</li>
+				<li>Retire young</li>
+			</ul>
+		</div>
+		<footer id="foot">
+			&copy 2016
+		</footer>
+
+	</body>
+</html>
